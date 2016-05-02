af10558aea3b07230a8ff00122b3c7d53c8124d4
Philip Lombardi <plombardi@datawire.io>
2016-05-02 16:00:15 -0400

    Jenkins cloud init

--- roles/cloud-init/files/cloud-init.sh
+++ roles/cloud-init/files/cloud-init.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 set -u
 
 arw_msg() {
@@ -7,9 +6,34 @@ arw_msg() {
   printf "%s\n" "--> $content"
 }
 
+config_bucket=${1:?Jenkins config bucket not specified!}
+jenkins_home=${2:-/var/lib/jenkins}
+
 arw_msg "Adding GitHub to SSH known hosts"
 ssh-keyscan -H github.com > /etc/ssh/ssh_known_hosts
 
-# TODO(@plombardi): This is where Jenkins master backup restoration logic should go...
+latest_backup="$(aws s3 ls s3://${config_bucket}/jenkins/backup --recursive | tail -n1 | rev | cut -d' ' -f1 | rev)"
 
+if [ "$latest_backup" != "jenkins/backup/" ]; then
+    arw_msg "Restoring from latest backup... (backup: ${latest_backup}"
+    rm -rf ${jenkins_home}/*
+    mkdir -p ${jenkins_home}
+
+    latest_backup_file="$(echo ${latest_backup} | rev | cut -d'/' -f1 | rev)"
+    arw_msg "Download latest Jenkins backup (file: ${latest_backup_file})"
+    aws s3 cp "s3://${config_bucket}/${latest_backup}" /tmp/${latest_backup_file}
+
+    service jenkins stop
+    arw_msg "Extracting Jenkins backup (into: $jenkins_home)"
+    cd ${jenkins_home}
+    cp /tmp/${latest_backup_file} ${jenkins_home}/${latest_backup_file}
+    tar -xzvf ${latest_backup_file} --strip 1
+    rm ${latest_backup_file}
+
+    chown -R jenkins:jenkins ${jenkins_home}
+else
+    arw_msg "No backup found to restore from; Clean install."
+fi
+
+service jenkins restart
 arw_msg "Done!"
